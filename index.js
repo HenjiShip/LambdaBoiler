f9228f422f06ae16b863199818a453d930b9017c
unknown <lihenry.cs@gmail.com>
2023-08-15 14:21:25 -0400

    added local and serverless deployment

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
-// const port = 3000;
+const port = 3000;
 
 app.use(express.json());
-console.log("hello world");
 
 app.get("/hello", (req, res) => {
   res.send("hello world");
 });
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
-
-module.exports.handler = serverless(app);
+// if for local testing and else for serverless-http
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+} else {
+  module.exports.handler = serverless(app);
+}
