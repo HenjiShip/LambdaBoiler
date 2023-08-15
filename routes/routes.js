f619d0b395f3d0a3b75be4e737e2509697eca184
unknown <lihenry.cs@gmail.com>
2023-08-15 15:24:44 -0400

    changed file name for easier comprehension

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { helloWorld, goodBye } = require("../controllers/getRoutes");
+const { helloWorld, goodBye } = require("../controllers/routeFunctions");
 
 router.get("/hello", helloWorld);
 router.get("/goodbye", goodBye);
